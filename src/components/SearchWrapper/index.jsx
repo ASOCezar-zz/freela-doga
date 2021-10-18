1b4a8750bb145c2fbb4706f3802439740e5b2c0f
asocezar <aso.cezar00@gmail.com>
2021-10-18 17:15:56 -0300

    Use ref add

--- src/components/SearchWrapper/index.jsx
+++ src/components/SearchWrapper/index.jsx
@@ -2,15 +2,18 @@ import { Icon, Input, Wrapper } from './styles';
 
 import PropTypes from 'prop-types';
 import searchIcon from '../../assets/search.svg'
+import { useRef } from 'react';
 
 export const SearchWrapper = ({ onChange }) => {
+  const searchInput = useRef(null);
+
   const focusInput = () => {
-    document.getElementById("search-input").focus();
+    searchInput.current?.focus();
   }
 
   return(
     <Wrapper>
-      <Input type="text" id="search-input" onChange={(e) => onChange(e)}/>
+      <Input type="text" id="search-input" ref={searchInput} onChange={(e) => onChange(e)}/>
       <Icon src={searchIcon} alt="Search Icon" onClick={focusInput}/>
     </Wrapper>
   )
